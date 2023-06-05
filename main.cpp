2a6ff4283466930f592ea37aa848310984b5e1a8
Aayush <aayushman950@gmail.com>
2023-06-05 11:35:26 +0545

    graph

--- main.cpp
+++ main.cpp
@@ -4,18 +4,24 @@
 int main() {
     Graph graph(false);
 
+    std::cout << "isEmpty(): " << graph.isEmpty() << std::endl;
+
     graph.addVertex(0);
     graph.addVertex(1);
     graph.addVertex(2);
     graph.addVertex(3);
+    std::cout << "Added vertices: 0, 1, 2, 3" << std::endl;
 
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
     graph.addEdge(2, 3);
     graph.addEdge(3, 0);
+    std::cout << "Added edges: (0,1)  (1,2)  (2,3)  (3,0)" << std::endl;
 
     std::cout << "Number of vertices: " << graph.NumVertices() << std::endl;
     std::cout << "Number of edges: " << graph.numEdges() << std::endl;
+
+    std::cout << "isEmpty(): " << graph.isEmpty() << std::endl;
 
     return 0;
 }
